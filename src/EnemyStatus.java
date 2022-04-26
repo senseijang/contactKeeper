f40c85d8a005d60fb15ad0bef3c3d52758fbe6d4
andyjang <andyjang@tesla.cs.iupui.edu>
2022-04-26 01:16:48 -0400

    added custom Attributes

---
+++ src/EnemyStatus.java
@@ -0,0 +1,16 @@
+/*
+ * EnemyStatus.java
+ *
+ * EnemyStatus is a child class of Attribute to store how unfriendly you are with someone.
+ *
+ * @author Sunwoo Jang
+ * @version April 26, 2022
+ */
+
+public class EnemyStatus extends Attribute {
+    public EnemyStatus () {
+        super(1, "annoying", "enemy", "hated");
+
+    }
+
+}
---
+++ src/FriendStatus.java
@@ -0,0 +1,16 @@
+/*
+ * FriendStatus.java
+ *
+ * FriendStatus is a child class of Attribute and will contain all states of friendship.
+ *
+ * @version April 26, 2022
+ * @author Sunwoo Jang
+ */
+
+public class FriendStatus extends Attribute {
+    public FriendStatus() {
+        super(1, "familiar", "friends", "best friends");
+
+    }
+
+}
